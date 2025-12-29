1c67e8b319c062b8e632f64d7b7b2ffa4c776ed9
nameispass <nvchiht@gmail.com>
2025-12-29 19:37:47 +0700

    aâ291

--- test/src/home.js
+++ test/src/home.js
@@ -1,5 +1,8 @@
 function execute() {
     return Response.success([
-        {title: "HOME", input: "https://www.tvtruyen.com", script: "gen.js"}
+        {title: "Truyện hot", input: "https://www.tvtruyen.com/the-loai/tat-ca/truyen-hot.html", script: "gen.js"},
+        {title: "Truyện độc quyền", input: "https://www.tvtruyen.com/the-loai/tat-ca/vip.html", script: "gen.js"},
+        {title: "Truyện đã hoàn thành", input: "https://www.tvtruyen.com/the-loai/tat-ca/truyen-full.html", script: "gen.js"},
+        {title: "Truyện mới cập nhật", input: "https://www.tvtruyen.com/the-loai/tat-ca/truyen-moi.html", script: "gen.js"}
     ]);
 }
